97bb1a4e18f19cc98fe26cab8a465954ba832817
Iván <ivantbd87@gmail.com>
2022-11-27 20:20:39 +0100

    CV updated

--- src/components/UI/speed-dial-component/SpeedDialComponent.jsx
+++ src/components/UI/speed-dial-component/SpeedDialComponent.jsx
@@ -83,7 +83,7 @@ const actions = [
   },
   {
     icon: (
-      <a href="https://drive.google.com/file/d/1OJWr_g44MP5hRPQKWLuVi79toDwg101T/view?usp=share_link" target="_blank" rel="noreferrer" 
+      <a href="https://drive.google.com/file/d/1dDqQMniRgZ93DxpajMeTCcfF5vy2nwhP/view?usp=sharing" target="_blank" rel="noreferrer" 
       className="text-dark text-decoration-none">
       <ContactPageTwoToneIcon />
       </a>
