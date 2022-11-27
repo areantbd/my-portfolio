0370bc729e327efdb34ef30250596b1afc466584
Iván <ivantbd87@gmail.com>
2022-11-27 20:09:58 +0100

    minor

--- src/data/certificates/certificatesData.js
+++ src/data/certificates/certificatesData.js
@@ -2,8 +2,8 @@ const certificatesData = [
     { name: "Web Development", entity: "Ironhack", date: "Oct-2022", image: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/60922403"},
     { name: "React + Redux", entity: "SoloLearn", date: "10-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-VS9EMUNW/jpg"},
     { name: "JavaScript", entity: "SoloLearn", date: "08-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-CAND3KS7/png"},
-    { name: "CSS", entity: "SoloLearn", date: "27-Jan-2022", image: "https://www.sololearn.com/Certificate/1014-14600015/jpg"},
-    { name: "HTML", entity: "SoloLearn", date: "06-Apr-2022", image: "https://www.sololearn.com/Certificate/1023-14600015/jpg"},
+    { name: "CSS", entity: "SoloLearn", date: "27-Jan-2022", image: "https://www.sololearn.com/Certificate/1023-14600015/jpg"},
+    { name: "HTML", entity: "SoloLearn", date: "06-Apr-2022", image: "https://www.sololearn.com/Certificate/1014-14600015/jpg"},
 ]
 
 module.exports = certificatesData
