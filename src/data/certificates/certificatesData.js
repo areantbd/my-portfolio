c322b8f2d0f26b61ccb5f3a928b97d5504d23c3b
Iván <ivantbd87@gmail.com>
2022-11-29 21:44:55 +0100

    certificates updated

--- src/data/certificates/certificatesData.js
+++ src/data/certificates/certificatesData.js
@@ -1,5 +1,6 @@
 const certificatesData = [
     { name: "Web Development", entity: "Ironhack", date: "Oct-2022", image: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/60922403"},
+    { name: "Responsive Web Design", entity: "SoloLearn", date: "29-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-QAIIGMLT/jpg"},
     { name: "React + Redux", entity: "SoloLearn", date: "10-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-VS9EMUNW/jpg"},
     { name: "JavaScript", entity: "SoloLearn", date: "08-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-CAND3KS7/png"},
     { name: "CSS", entity: "SoloLearn", date: "27-Jan-2022", image: "https://www.sololearn.com/Certificate/1023-14600015/jpg"},
