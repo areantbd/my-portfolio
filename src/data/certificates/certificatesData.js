0fd4df5a7cf8dd47bb15653d7afe0c112310bd43
Iván <ivantbd87@gmail.com>
2023-04-11 21:44:21 +0200

    update: certificates updated

--- src/data/certificates/certificatesData.js
+++ src/data/certificates/certificatesData.js
@@ -1,4 +1,6 @@
 const certificatesData = [
+    { name: "Introduction to SQL", entity: "SoloLearn", date: "08-Apr-2023", image: "https://res.cloudinary.com/dqzakvyfj/image/upload/v1681242159/Certificados/Introduction_to_SQL_tyfxi3.jpg"},
+    { name: "JavaScript Intermed.", entity: "SoloLearn", date: "05-Apr-2023", image: "https://res.cloudinary.com/dqzakvyfj/image/upload/v1681242158/Certificados/JS_intermediate_euq0hk.jpg"},
     { name: "Web Development", entity: "Ironhack", date: "Oct-2022", image: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/60922403"},
     { name: "Responsive Web Design", entity: "SoloLearn", date: "29-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-QAIIGMLT/jpg"},
     { name: "React + Redux", entity: "SoloLearn", date: "10-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-VS9EMUNW/jpg"},
