edb0e7bcd6bfffc0fdd9fa217e117eb61925270b
Iván <ivantbd87@gmail.com>
2023-06-12 23:17:55 +0200

    certificates updated

--- src/data/certificates/certificatesData.js
+++ src/data/certificates/certificatesData.js
@@ -1,12 +1,13 @@
 const certificatesData = [
+    { name: "Scrum Fundamentals", entity: "ScrumStudy", date: "12-Jun-2023", image: "https://res.cloudinary.com/dqzakvyfj/image/upload/v1686603338/Scrum_fundamentals_del7lp.png"},
     { name: "Introduction to SQL", entity: "SoloLearn", date: "08-Apr-2023", image: "https://res.cloudinary.com/dqzakvyfj/image/upload/v1681242159/Certificados/Introduction_to_SQL_tyfxi3.jpg"},
     { name: "JavaScript Intermed.", entity: "SoloLearn", date: "05-Apr-2023", image: "https://res.cloudinary.com/dqzakvyfj/image/upload/v1681242158/Certificados/JS_intermediate_euq0hk.jpg"},
     { name: "Web Development", entity: "Ironhack", date: "Oct-2022", image: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/60922403"},
     { name: "Responsive Web Design", entity: "SoloLearn", date: "29-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-QAIIGMLT/jpg"},
     { name: "React + Redux", entity: "SoloLearn", date: "10-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-VS9EMUNW/jpg"},
     { name: "JavaScript", entity: "SoloLearn", date: "08-Nov-2022", image: "https://www.sololearn.com/Certificate/CT-CAND3KS7/png"},
-    { name: "CSS", entity: "SoloLearn", date: "27-Jan-2022", image: "https://www.sololearn.com/Certificate/1023-14600015/jpg"},
     { name: "HTML", entity: "SoloLearn", date: "06-Apr-2022", image: "https://www.sololearn.com/Certificate/1014-14600015/jpg"},
+    { name: "CSS", entity: "SoloLearn", date: "27-Jan-2022", image: "https://www.sololearn.com/Certificate/1023-14600015/jpg"},
 ]
 
 module.exports = certificatesData
