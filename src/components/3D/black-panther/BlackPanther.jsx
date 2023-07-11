a6f0c4f578d5ac85fb749e174512650e5dfd2f16
Iván <ivantbd87@gmail.com>
2023-07-11 22:35:07 +0200

    feat: create some 3D models, and add some interactions on scroll

--- src/components/3D/black-panther/BlackPanther.jsx
+++ src/components/3D/black-panther/BlackPanther.jsx
@@ -1,14 +1,47 @@
 
-import React from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { Canvas, useFrame } from 'react-three-fiber'
 
-export default function BlackPanther(props) {
+function Model(props) {
+  let xPosition = props.pos / 200
   const { nodes, materials } = useGLTF('/disney_infinity_3.0_-_avg_black_panther-transformed.glb')
+  const meshRef = useRef()
+  useFrame((state, delta) => (meshRef.current.rotation.y = xPosition))
+  // useFrame((state, delta) => (meshRef.current.rotation.x += delta/10))
+  
+  const [hovered, setHover] = useState(false)
+  console.log(hovered)
   return (
-    <group {...props} dispose={null}>
-      <mesh geometry={nodes.AVG_Black_Panther_Body_AVG_Black_Panther_0.geometry} material={materials.AVG_Black_Panther} scale={0.885} />
+    <group ref={meshRef} {...props} dispose={null}>
+      <mesh 
+        geometry={nodes.AVG_Black_Panther_Body_AVG_Black_Panther_0.geometry} 
+        material={materials.AVG_Black_Panther} 
+        scale={2}           
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}>
+      </mesh>
+        
     </group>
   )
 }
 
-useGLTF.preload('/disney_infinity_3.0_-_avg_black_panther-transformed.glb')
+function BlackPanther({ pos }) {
+  return (
+    <div style={{width: "100%", height: "80vh", zIndex: 0}} className="mx-auto render">
+      <Canvas camera={{zoom: 20, position: [0, -10, 90], fov: 50}}>
+        <ambientLight intensity={0.9} />
+        <pointLight position={[135, 135, 0]} intensity={3} color={"blue"} />
+        <pointLight position={[-135, 135, 0]} intensity={5} color={"red"} />
+        <Suspense fallback={null}>
+          <Model pos={pos} position={[0, 0, -20]}/>
+        </Suspense>
+        {/* <OrbitControls /> */}
+      </Canvas>
+    </div>
+  )
+}
+
+export default BlackPanther
+
+// useGLTF.preload('/disney_infinity_3.0_-_avg_black_panther-transformed.glb')
---
+++ src/components/3D/bmw/BMW.jsx
@@ -0,0 +1,82 @@
+import React, { Suspense, useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { Canvas, useFrame } from 'react-three-fiber'
+
+export function Model(props) {
+  let Position = props.pos / 200
+  const { nodes, materials } = useGLTF('/free_bmw_m3_e30.glb')
+  const meshRef = useRef()
+  useFrame((state, delta) => (meshRef.current.rotation.y = -Position))
+  useFrame((state, delta) => (meshRef.current.rotation.x = Position/50))
+  
+  return (
+    <group ref={meshRef} {...props} dispose={null}>
+      <group position={[0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]}>
+        <mesh geometry={nodes.Object_21.geometry} material={materials.Brembo_Calipers} />
+        <mesh geometry={nodes.Object_23.geometry} material={materials.Logo_Plane} rotation={[0, -0.501, 0]} />
+      </group>
+      <group position={[-0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]}>
+        <mesh geometry={nodes.Object_31.geometry} material={materials.Brembo_Calipers} />
+        <mesh geometry={nodes.Object_33.geometry} material={materials.Logo_Plane} rotation={[-Math.PI, -0.354, 0]} />
+      </group>
+      <group position={[0.709, 0.311, -1.241]}>
+        <mesh geometry={nodes.Object_41.geometry} material={materials.Brembo_Calipers} />
+        <mesh geometry={nodes.Object_43.geometry} material={materials.Logo_Plane} />
+      </group>
+      <group position={[-0.709, 0.311, -1.241]}>
+        <mesh geometry={nodes.Object_51.geometry} material={materials.Brembo_Calipers} />
+        <mesh geometry={nodes.Object_53.geometry} material={materials.Logo_Plane} />
+      </group>
+      <mesh geometry={nodes.Object_4.geometry} material={materials.BMW_E30_M3_PAINT} />
+      <mesh geometry={nodes.Object_5.geometry} material={materials.BMW_E30_M3_PLASTIC} />
+      <mesh geometry={nodes.Object_6.geometry} material={materials.BMW_E30_M3_CHROME} />
+      <mesh geometry={nodes.Object_7.geometry} material={materials.BMW_E30_M3_WINDOWS} />
+      <mesh geometry={nodes.Object_8.geometry} material={materials.BMW_E30_M3_BLACKOUT} />
+      <mesh geometry={nodes.Object_9.geometry} material={materials.BMW_E30_M3_SIDE_MIRROR} />
+      <mesh geometry={nodes.Object_10.geometry} material={materials.BMW_E30_M3_TAILLIGHT_REFLECTOR} />
+      <mesh geometry={nodes.Object_11.geometry} material={materials.BMW_E30_M3_HEADLIGHT_REFLECTOR} />
+      <mesh geometry={nodes.Object_12.geometry} material={materials.BMW_E30_M3_EMBLEMS} />
+      <mesh geometry={nodes.Object_13.geometry} material={materials.BMW_E30_M3_LENS} />
+      <mesh geometry={nodes.Object_15.geometry} material={materials.BMW_E30_M3_RIM} position={[0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]} />
+      <mesh geometry={nodes.Object_17.geometry} material={materials.BMW_E30_M3_TIRE} position={[0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]} />
+      <mesh geometry={nodes.Object_19.geometry} material={materials.Brake_Disc} position={[0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]} />
+      <mesh geometry={nodes.Object_25.geometry} material={materials.BMW_E30_M3_RIM} position={[-0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]} />
+      <mesh geometry={nodes.Object_27.geometry} material={materials.BMW_E30_M3_TIRE} position={[-0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]} />
+      <mesh geometry={nodes.Object_29.geometry} material={materials.Brake_Disc} position={[-0.697, 0.311, 1.35]} rotation={[0, -0.501, 0]} />
+      <mesh geometry={nodes.Object_35.geometry} material={materials.BMW_E30_M3_RIM} position={[0.709, 0.311, -1.241]} />
+      <mesh geometry={nodes.Object_37.geometry} material={materials.BMW_E30_M3_TIRE} position={[0.709, 0.311, -1.241]} />
+      <mesh geometry={nodes.Object_39.geometry} material={materials.Brake_Disc} position={[0.709, 0.311, -1.241]} />
+      <mesh geometry={nodes.Object_45.geometry} material={materials.BMW_E30_M3_RIM} position={[-0.709, 0.311, -1.241]} />
+      <mesh geometry={nodes.Object_47.geometry} material={materials.BMW_E30_M3_TIRE} position={[-0.709, 0.311, -1.241]} />
+      <mesh geometry={nodes.Object_49.geometry} material={materials.Brake_Disc} position={[-0.709, 0.311, -1.241]} />
+    </group>
+  )
+}
+function BMW ({ pos, zoom }) {
+  // const [responsiveZoom, setResponsiveZoom] = useState()
+  // useEffect(() => {
+  //   if (window.innerWidth < 900 ){
+  //     setResponsiveZoom(20)
+  //   }else {
+  //     setResponsiveZoom(50)
+  //   }
+  // }, [zoom])
+  return (
+    <div style={{width: "100%", height: "80vh", zIndex: 0}} className="render">
+    
+      <Canvas camera={{zoom: 50, position: [0, 0, 90], fov: 50}}>
+        <ambientLight intensity={0.9} />
+        <pointLight position={[135, 135, 0]} intensity={0.5} color={"blue"} />
+        <pointLight position={[0, 135, 0]} intensity={0.5} color={"white"} />
+        <pointLight position={[-135, 135, 0]} intensity={1} color={"blue"} />
+        <pointLight position={[-135, 135, 0]} intensity={1} color={"white"} />
+        <Suspense fallback={null}>
+          <Model pos={pos} position={[0, -0.5, -10]}/>
+        </Suspense>
+        {/* <OrbitControls /> */}
+      </Canvas>
+    </div>
+  )
+}
+
+export default BMW 
