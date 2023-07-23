68c43542c0ab49a7c0d910c58da6833a09a6f3c2
Iván <ivantbd87@gmail.com>
2023-07-23 18:06:10 +0200

    fix: fixed typed component

--- src/components/UI/typed/TypedComponent.jsx
+++ src/components/UI/typed/TypedComponent.jsx
@@ -1,32 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TypeAnimation } from 'react-type-animation'
 
-function TypedComponent() {
+function TypedComponent({text, time}) {
+  const [typingStatus, setTypingStatus] = useState('Initializing');
+  console.log(typingStatus)
   return (
     <div className='d-flex flex-column justify-content-center align-items-center pt-3'>
-    <h2 style={{fontSize: "1.8rem", fontWeight: "bold", filter: "drop-shadow(2px 2px 2px #4d4f53)"}}>I have knowledge about: </h2>
     <TypeAnimation 
+      // sequence={[text, time] || [
+      //   'React', 1500,
+      //   'JavaScript', 1500,
+      //   'TypeScript', 1500,
+      //   'NodeJS', 1500,
+      //   'Git', 1500,
+      //   'GitHub', 1500,
+      //   'BootStrap', 1500,
+      //   'Express', 1500,
+      //   'MongoDB', 1500,
+      //   'Postman', 1500,
+      //   'React Native', 1500,
+      //   'HTML', 1500,
+      //   'CSS', 1500,
+      //   'SASS', 1500,
+      //   ]}
+      // speed={10}
+      // deletionSpeed={50}
+      // cursor={true}
+      // repeat={Infinity}
+      // style={{fontSize: "1.8rem", color: "#3178c6", fontWeight: "bold", filter: "drop-shadow(2px 2px 2px #4d4f53)"}}
+
       sequence={[
-        'React', 1500,
-        'JavaScript', 1500,
-        'TypeScript', 1500,
-        'NodeJS', 1500,
-        'Git', 1500,
-        'GitHub', 1500,
-        'BootStrap', 1500,
-        'Express', 1500,
-        'MongoDB', 1500,
-        'Postman', 1500,
-        'React Native', 1500,
-        'HTML', 1500,
-        'CSS', 1500,
-        'SASS', 1500,
-        ]}
-      speed={10}
-      deletionSpeed={50}
-      cursor={true}
-      repeat={Infinity}
-      style={{fontSize: "1.8rem", color: "#3178c6", fontWeight: "bold", filter: "drop-shadow(2px 2px 2px #4d4f53)"}}
+    1500,
+    () => {
+      setTypingStatus('Typing...');
+    },
+    text,
+    () => {
+      setTypingStatus('Done Typing');
+    },
+    1000,
+    () => {
+      setTypingStatus('Deleting...');
+    },
+    '',
+    () => {
+      setTypingStatus('Done Deleting');
+    },
+  ]}
+  speed={10}
+  deletionSpeed={50}
+  repeat={Infinity}
+  style={{fontSize: "1.8rem", color: "#3178c6", fontWeight: "bold", filter: "drop-shadow(2px 2px 2px #4d4f53)"}}
     />
     </div>
   )
--- src/screens/Github.jsx
+++ src/screens/Github.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Skyline2022 from '../components/3D/gh-skyline-2022/Skyline2022'
 import NavBar from '../components/UI/nav-bar/NavBar'
+import TypedComponent from '../components/UI/typed/TypedComponent'
 
 function Github() {
   return (
     <>
       <NavBar />
-      <div className='container pt-3'>
+      <div className='container pt-3 pb-5'>
         <a href='https://skyline.github.com/' target='_blank' rel="noreferrer" className='btn btn-outline-info btn-sm' >Create your own skyline</a>
         <Skyline2022 />
-        <h1 className='text-center'>More content soon...</h1>
+        <TypedComponent text={"More content soon..."} time={1000}/>
       </div>
     </>
   )
